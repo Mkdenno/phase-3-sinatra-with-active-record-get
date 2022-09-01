c8254661b0cd505d83e7260b8f106b2560194a4a
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-09-01 18:10:25 +0300

    commit tree 75c2bb1b1cc8e768d80ab19214f142f94cad3394

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+#add this line to set the Content-type header fr all responses
+set :default_content_type, 'application/json'
+
+ get '/games' do
+   games=Game.all.order(:title).limit(10)
+   games.to_json
   end
 
+  # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    # send a JSON-formatted response of the game data
+    game=Game.find(params[:id])
+    # send a JSON-formatted response of the game data 
+  # include the associated reviews in the json responses
+  game.to_json(include: { reviews: { include: :user } })
+
+  end
 end
